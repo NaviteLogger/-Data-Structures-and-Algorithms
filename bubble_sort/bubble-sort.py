83a3b2f43690815e66ff7f9932363faa0e2f35ed
NavieteLogger <kacprzakmarek92@gmail.com>
2024-10-24 09:17:46 +0000

    Typo fix

--- bubble_sort/bubble-sort.py
+++ bubble_sort/bubble-sort.py
@@ -1,10 +1,14 @@
 def bubble_sort(array):
+    was_swapped = False
     zamiany = 0
     for i in range(len(array)):
         for j in range(len(array) - i-1):
             if array[j] > array[j+1]:
                 array[j], array[j+1] = array[j+1], array[j]
                 zamiany += 1
+                was_swapped = True
+        if not was_swapped:
+            break
     print(zamiany)
 
 
