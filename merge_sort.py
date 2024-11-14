b16c89743319dbbe2be8e202ac5986405371a561
NavieteLogger <kacprzakmarek92@gmail.com>
2024-11-14 10:40:29 +0000

    Added quicksort

---
+++ merge_sort.py
@@ -0,0 +1,33 @@
+def mergeSort(arr):
+    if len(arr) > 1:
+        m = len(arr) // 2
+        leftArray = arr[:m]
+        rightArray = arr[m:]
+
+        mergeSort(leftArray)
+        mergeSort(rightArray)
+
+        i = j = k = 0
+        while i < len(leftArray) and j < len(rightArray):
+            if leftArray[i] < rightArray[j]:
+                arr[k] = leftArray[i]
+                i += 1
+            else:
+                arr[k] = rightArray[j]
+                j += 1
+            k += 1
+
+        while i < len(leftArray):
+            arr[k] = leftArray[i]
+            i += 1
+            k += 1
+
+        while j < len(rightArray):
+            arr[k] = rightArray[j]
+            j += 1
+            k += 1
+
+
+array = [35, 22, 41, 4, 8, 81, 15]
+mergeSort(array)
+print(array)
